ba117ea980b546f8327725b98ad3893152f66751
gigiwu <ms.ggwu@gmail.com>
2017-08-11 09:20:30 -0700

    24,25

---
+++ ex24.py
@@ -0,0 +1,15 @@
+print("Let's practice")
+print('escape \' and \\ and\n and \t. That\'s it.')
+
+poem = """
+several line peom
+\tI am grateful.
+that i am healthy
+"""
+
+print("-----")
+print("Poem")
+print("-----")
+
+print("we can print like this:{}".format(poem))
+print(f"Or like this:{poem}")
